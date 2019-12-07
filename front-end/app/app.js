41b6947f4297fb0b14ac5ce51869a6714821745e
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2019-12-07 14:40:00 -0300

    initial commit project structure

---
+++ front-end/app/app.js
@@ -0,0 +1,11 @@
+var app = angular.module('app', [])
+app.controller('appController', ['$scope', function ($scope) {
+ 
+
+$scope.title = "it works"
+
+
+}])
+// appController.config(function () {
+
+// })
---
+++ front-end/gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const uglifycss = require('gulp-uglifycss');
+const browserSync = require('browser-sync').create();
+
+//compile sccs into css
+function style(){
+ // where is my css file
+  return gulp.src('./app/scss/*.scss')
+  // pass that file through sass compiler
+      .pipe(sass().on('error', sass.logError))
+      // where do I save the compiled css
+      .pipe(gulp.dest('./css'))
+    // stream changes to all browsers
+      .pipe(browserSync.stream())
+  }
+
+  function css(){
+    return  gulp.src('./css/*.css')
+          .pipe(uglifycss({
+            "uglyComments": true
+          }))
+          .pipe(gulp.dest('./dist/'));
+  }
+
+  function watch(){
+    browserSync.init({
+      server:{
+        baseDir: './'
+      }
+    });
+    gulp.watch('./app/scss/*.scss', style)
+    gulp.watch('./css/*.css', css)
+    gulp.watch('./*.html').on('change', browserSync.reload)
+    gulp.watch('./app/*.js').on('change', browserSync.reload)
+  }
+
+  exports.style = style;
+  exports.css = css;
+  exports.watch = watch;
+
+
+ 
