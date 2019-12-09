dd3b123cf5695c015bd38f1d4d278c47921eec4b
brunoalvesdasilvabr <brunoalvesdasilvabr@gmail.com>
2019-12-08 22:11:42 -0300

    navbar finished

--- front-end/app/app.js
+++ front-end/app/app.js
@@ -1,11 +1,15 @@
 var app = angular.module('app', [])
 app.controller('appController', ['$scope', function ($scope) {
  
+$scope.price = 20,00
 
-$scope.title = "it works"
-
+$scope.getProducts = function (){
+        axios('./assets/products.json')
+        .then(function(response){
+            console.log(response.data)
+        })
+}
+$scope.getProducts()
 
 }])
-// appController.config(function () {
 
-// })
--- front-end/gulpfile.js
+++ front-end/gulpfile.js
@@ -3,24 +3,26 @@ const sass = require('gulp-sass');
 const uglifycss = require('gulp-uglifycss');
 const browserSync = require('browser-sync').create();
 
-//compile sccs into css
-function style(){
- // where is my css file
-  return gulp.src('./app/scss/*.scss')
-  // pass that file through sass compiler
-      .pipe(sass().on('error', sass.logError))
-      // where do I save the compiled css
-      .pipe(gulp.dest('./css'))
-    // stream changes to all browsers
-      .pipe(browserSync.stream())
-  }
+// //compile sccs into css
+// function style(){
+//  // where is my css file
+//   return gulp.src('./app/scss/*.scss')
+//   // pass that file through sass compiler
+//       .pipe(sass().on('error', sass.logError))
+//       // where do I save the compiled css
+//       .pipe(gulp.dest('./css'))
+//     // stream changes to all browsers
+//       .pipe(browserSync.stream())
+//   }
 
   function css(){
-    return  gulp.src('./css/*.css')
+    return  gulp.src('./app/css/*.css')
           .pipe(uglifycss({
             "uglyComments": true
           }))
-          .pipe(gulp.dest('./dist/'));
+          .pipe(gulp.dest('./dist/'))
+         .pipe(browserSync.stream())
+
   }
 
   function watch(){
@@ -29,13 +31,13 @@ function style(){
         baseDir: './'
       }
     });
-    gulp.watch('./app/scss/*.scss', style)
-    gulp.watch('./css/*.css', css)
+    // gulp.watch('./app/scss/*.scss', style)
+    gulp.watch('./app/css/*.css', css)
     gulp.watch('./*.html').on('change', browserSync.reload)
     gulp.watch('./app/*.js').on('change', browserSync.reload)
   }
 
-  exports.style = style;
+  // exports.style = style;
   exports.css = css;
   exports.watch = watch;
 
